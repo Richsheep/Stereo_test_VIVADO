d0d0a66665091f6f45623e11325a7a22985e7dda
Richsheep <richsheep@hotmail.com>
2021-04-28 16:06:44 +0800

    Update stereo_test.cpp
    添加注释和格式整理

--- stereo_test.cpp
+++ stereo_test.cpp
@@ -1,25 +1,34 @@
 #include "stereo_top.h"
-//#include "opencv/cv.h"
-//#include "opencv/cxcore.h"
-//#include "opencv/highgui.h"
 #include "hls_opencv.h"
 
 using namespace hls;
 
-int main (int argc, char** argv) {
+int main (int argc, char** argv) 
+{
+    // 图像加载
     IplImage* src_l = cvLoadImage("tsukuba_l.png", CV_LOAD_IMAGE_COLOR);
-	IplImage* src_r = cvLoadImage("tsukuba_r.png", CV_LOAD_IMAGE_COLOR);
-    //IplImage* dst = cvCreateImage(cvGetSize(src_l), src_l->depth, src_l->nChannels);
+    IplImage* src_r = cvLoadImage("tsukuba_r.png", CV_LOAD_IMAGE_COLOR);
+    
+    // 输出图像初始化
     IplImage* dst = cvCreateImage(cvGetSize(src_l), IPL_DEPTH_8U, 3);
+
     AXI_STREAM_IN src_l_axi;
-	AXI_STREAM_IN src_r_axi;
+    AXI_STREAM_IN src_r_axi;
+    
     AXI_STREAM_OUT dst_axi;
-    IplImage2AXIvideo(src_l, src_l_axi); //��ͼ��ת��Ϊ��Ƶ���ṹ
-	IplImage2AXIvideo(src_r, src_r_axi);
+
+    // 将图像转换为AXI视频流结构
+    IplImage2AXIvideo(src_l, src_l_axi); 
+    IplImage2AXIvideo(src_r, src_r_axi);
+    
+    // 计算深度图
     hls_stereoBM(src_l_axi, src_r_axi, dst_axi, src_l->height, src_r->width);
+	
+    // AXI视频流结构转换为图像
     AXIvideo2IplImage(dst_axi, dst);
+
+    // 保存结果图像
     cvSaveImage("OUTPUT_IMAGE.png", dst);
-	//cvShowImage("Disparity", dst);
 
     cvReleaseImage(&src_l);
     cvReleaseImage(&src_r);
